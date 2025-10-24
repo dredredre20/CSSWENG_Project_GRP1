da42a8d9cdd18c9c60fbb67c7f136a1670f984ab
nite29 <ardjunsay29@gmail.com>
2025-10-24 15:04:46 +0800

    refactored code and hashed passwords

--- seed_db.js
+++ seed_db.js
@@ -1,48 +1,50 @@
 // here be dummy users to experiment with
+import bcrypt from "bcrypt";
 import db_connection_pool from "./connections.js";
 
 async function insert_dummy_users(){
     // here are the list of dummy users, feel free to change and/or add more users
     const samples = [
-        {staffid: 10000, stafftype: "A", Name:"John Doe", email: "admin1@gmail.com", password: "password123"},
-        {staffid: 10001, stafftype: "A", Name:"Jane Beck", email: "admin2@gmail.com", password: "password123"},
-        {staffid: 10002, stafftype: "S", Name:"Jenny Parker", email: "visor1@gmail.com", password: "password123"},
-        {staffid: 10003, stafftype: "S", Name:"Wesley Ang", email: "visor2@gmail.com", password: "password123"},
-        {staffid: 10004, stafftype: "D", Name:"Angelo Perdo", email: "sdw1@gmail.com", password: "password123"},
-        {staffid: 10005, stafftype: "D", Name:"Jane Newbabel", email: "sdw2@gmail.com", password: "password123"}
-    ]
+        { stafftype: "admin", first_name: "John", last_name: "Doe", email: "admin1@gmail.com", password: "password123" },
+        { stafftype: "admin", first_name: "Jane", last_name: "Beck", email: "admin2@gmail.com", password: "password123" },
+        { stafftype: "supervisor", first_name: "Jenny", last_name: "Parker", email: "visor1@gmail.com", password: "password123" },
+        { stafftype: "supervisor", first_name: "Wesley", last_name: "Ang", email: "visor2@gmail.com", password: "password123" },
+        { stafftype: "sdw", first_name: "Angelo", last_name: "Perdo", email: "sdw1@gmail.com", password: "password123" },
+        { stafftype: "sdw", first_name: "Jane", last_name: "Newbabel", email: "sdw2@gmail.com", password: "password123" }
+    ];
 
     //get connection
     const connection = await db_connection_pool.getConnection();
 
     try{
-        // pre_check is statement for checking existence ofd account beforehand
-        const pre_check = 'SELECT * FROM new_reports_db.staffinfo WHERE email = ? AND password = ?';
+        for (const user of samples) {
+            const hashed = await bcrypt.hash(user.password, 10);
+            let query = "";
+            let values = [];
 
-        // actual INSERT
-        const insert = 'INSERT INTO new_reports_db.staffinfo (staffid, stafftype, Name, email, password)  VALUES(?, ?, ?, ?, ?)'
-        
-        //iterate through each user account object in `samples`
-        console.log("\nUsers:");
-        for(let user of samples){
-            // check first
-            const [rows] = await connection.execute(pre_check, [user.email, user.password]); 
-            
-            if(rows.length > 0){
-                console.log(`(${user.stafftype}) email: ${user.email}, password: ${user.password}`);
-            } else{
-                // ..only then INSERT when account not found
-                await connection.execute(insert, [user.staffid, user.stafftype, user.Name, user.email, user.password]); 
-                console.log(`(${user.stafftype}) email: ${user.email}, password: ${user.password}`);
+            switch(user.stafftype) {
+                case "admin":
+                    query = 'INSERT INTO admins (first_name, last_name, email, password) VALUES (?, ?, ?, ?)';
+                    values = [user.first_name, user.last_name, user.email, hashed];
+                    break;
+                case "supervisor":
+                    query = 'INSERT INTO supervisors (first_name, last_name, email, password) VALUES (?, ?, ?, ?)';
+                    values = [user.first_name, user.last_name, user.email, hashed];
+                    break;
+                case "sdw":
+                    query = 'INSERT INTO sdws (first_name, last_name, email, password) VALUES (?, ?, ?, ?)';
+                    values = [user.first_name, user.last_name, user.email, hashed];
+                    break;
             }
+            await connection.execute(query, values);
         }
-        console.log("\n");
+        console.log("Sucessfully seeded dummy users.")
+
     } catch(err){
         console.error("Error seeding users: " + err);
     } finally{
         connection.release();
     }
 }
-
 
 export default insert_dummy_users;
