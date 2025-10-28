14c5da1b99cdbbbc6256be80bf1347cd4f2885b8
jingojam <xtian.arano1224@gmail.com>
2025-10-28 23:33:36 +0800

    reused sdw_homepage to current sdw_upload_handler

--- public/js/sdw_homepage.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.nav-btn').forEach(btn => {
-    const category = btn.dataset.category;
-
-    if (category) {
-        btn.addEventListener('click', () => {
-            window.location.href = `/reports/${encodeURIComponent(category)}`;
-        });
-    }
-});
