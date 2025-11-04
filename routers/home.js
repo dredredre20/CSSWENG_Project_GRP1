c3ec9fccf89c01a6dea2ecb04ec578315a9946d0
jingojam <xtian.arano1224@gmail.com>
2025-11-04 18:15:32 +0800

    undoing changes

--- routers/home.js
+++ routers/home.js
@@ -1,30 +1,47 @@
+import db_connection_pool from '../connections.js';
 import express from 'express';
 
 const homeRouter = express.Router();
 
-homeRouter.get('/', (req, res) => {
+// get all sdws under the specific supervisor
+async function getSdws(connection, supervisor_id){
+    try{
+        const [sdws] = await connection.execute(
+            'SELECT * FROM sdws WHERE supervisor_id = ?',
+            [supervisor_id]
+        );
+        return sdws;
+    } catch(err){
+        console.error('ERROR in login.js getSdws() function: ' + err);
+    }
+}
+
+homeRouter.get('/', async (req, res) => {
     //if the user is in session,, only
     if(req.session.logged_user){
+        const connection = await db_connection_pool.getConnection();
         // obtain the logged user in the session
         const user = req.session.logged_user;
         //here just pass stuff to render in the page based on role
         if(user.staff_type === 'admin'){
             res.render('home', { 
-                user: user,
-                role: 'Admin Dashboard',
+                user: user
             });
         } else if(user.staff_type === 'supervisor'){ 
-
+            // for supervisor, include the list of sdws under them for rendering
+            const sdws = await getSdws(connection, user.id);
             res.render('supervisor_homepage', { //renders supervisor_homepage.ejs
                 user: user,
-                role: 'Supervisor Dashboard',
+                sdws: sdws
             });
         } else if(user.staff_type === 'sdw'){
             res.render('sdw_homepage', {  // route to sdw_homepage.ejs page
-                user: user,
-                role: 'User Dashboard',
+                user: user
             });
         }
+
+        await connection.release();
+
     } else {
         //if no user just go back to /login route
         res.redirect('/login');
