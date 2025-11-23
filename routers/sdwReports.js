0e10ffb8a11560c7b0023c929888c2a8b5cc76b6
jingojam <xtian.arano1224@gmail.com>
2025-11-23 17:18:10 +0800

    fixed supabase import

--- routers/sdwReports.js
+++ routers/sdwReports.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { supabase } from '../client.js';
+import { supabase } from '../middleware/supabase_client.js';
 
 const reportRouter = express.Router();
 const supervisorSdwReportRouter = express.Router();
